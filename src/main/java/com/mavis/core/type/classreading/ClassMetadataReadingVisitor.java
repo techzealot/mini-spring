22e064843b672a62d37582dfd7b9a8bf50c8ff3c
techzealot <techzealot@foxmail.com>
2018-06-12 13:04:34 +0800

    添加spock单元测试配置

--- src/main/java/com/mavis/core/type/classreading/ClassMetadataReadingVisitor.java
+++ src/main/java/com/mavis/core/type/classreading/ClassMetadataReadingVisitor.java
@@ -9,4 +9,82 @@ public class ClassMetadataReadingVisitor extends ClassVisitor implements ClassMe
 	public ClassMetadataReadingVisitor() {
 		super(Opcodes.ASM6);
 	}
+
+	@Override
+	public String getClassName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isInterface() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAnnotation() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAbstract() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isConcrete() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isFinal() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isIndependent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean hasEnclosingClass() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getEnclosingClassName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasSuperClass() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getSuperClassName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getInterfaceNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getMemberClassNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
